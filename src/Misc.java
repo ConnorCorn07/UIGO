a373af4dd7c52595b793dcb4fd68523e399b0311
nicknamee12 <supermegalegos@gmail.com>
2026-09-23 15:57:04 -0500

    cleanup

--- src/Misc.java
+++ src/Misc.java
@@ -2,23 +2,23 @@ import java.util.Scanner;
 
 public class Misc {
 
-    public static int getBet(Scanner sysin) throws InterruptedException {
+    public static int getBet(Scanner sysin) {
         return getBet(sysin, 0);
     }
 
-    public static int getBet(Scanner sysin, int max) throws InterruptedException {
-        int bet = 0;
+    public static int getBet(Scanner sysin, int max) {
+        int bet;
         while (true) {
             System.out.print(colors.TEXT_BRIGHT_WHITE + "Place your bet");
             if (max > 0) {
                 System.out.println(" | Max : " + max);
             } else { System.out.println(); }
-            Thread.sleep(150);
+
             System.out.println(colors.TEXT_BRIGHT_WHITE + "You have " + colors.TEXT_BRIGHT_GREEN + Main.money + colors.TEXT_BRIGHT_WHITE + " Chips");
 
             try {
                 bet = Integer.parseInt(sysin.nextLine());
-            } catch (NumberFormatException e) {}
+            } catch (NumberFormatException e) { bet = 0; }
 
             if (!checksum.bet(bet) || (max > 0 && bet > max)) {
                 System.out.println(colors.TEXT_RED + "That is not a valid input");
@@ -35,7 +35,6 @@ public class Misc {
     public static void execBet(int bet) {
         if (checksum.bet(bet)) {
             Main.money -= bet;
-            return;
         } else {
             throw new RuntimeException("Unable to execute bet");
         }
