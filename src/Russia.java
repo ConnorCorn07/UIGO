e8d71692c70eee610c1b1c0e157fab0ff369d22a
nicknamee12 <supermegalegos@gmail.com>
2026-09-24 21:21:57 -0500

    Russian Roulette functional bet def not done

--- src/Russia.java
+++ src/Russia.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 
 public class Russia {
 
+    protected static final int winPayout = 2;
+    protected static final int losePayout = 10;
 
     public static void initial(Scanner sysin) throws InterruptedException{
 
@@ -17,8 +19,40 @@ public class Russia {
 
         String Choice1 = sysin.nextLine();
         if (Choice1.contains("y")){
-            int max = Math.max(1, Main.GetBalance() / 10);
-            Misc.getBet(sysin, max);
+            int max = Main.GetBalance() / 10;
+            if (max > 0) {
+                russiaGame(sysin, Misc.getBet(sysin, max));
+            }
+            System.out.println(Colors.TEXT_RED + "You don't have enough chips to play this game");
+            Main.gamemode(sysin);
         } else Main.gamemode(sysin);
+    }
+
+    private static void russiaGame(Scanner sysin, int bet) {
+        if (bet*losePayout > Main.GetBalance()) {
+            System.out.println(Colors.TEXT_RED + "You don't have enough chips left to cover that bet.");
+            return;
+        }
+        Misc.execBet(bet*losePayout);
+
+        Random random = new Random();
+        int round = random.nextInt(0, 6);
+        for (int i = 0; i <= round; i++) {
+            if (i == round) {
+                System.out.println("*Boom* You lost " + Colors.TEXT_RED + bet*losePayout + Colors.TEXT_BRIGHT_WHITE + " Chips to medical bills");
+                break;
+            } else {
+                System.out.print("*Click* Winnings : ");
+                System.out.println(bet * (int) Math.pow(winPayout, i + 1));
+                if (!sysin.nextLine().isEmpty()) {
+                    Main.AddBalance((int) (bet*losePayout + bet*Math.pow(winPayout, i + 1)));
+                    System.out.println("Win " + Main.GetBalance());
+                    break;
+                }
+            }
+
+        }
+
+
     }
 }
